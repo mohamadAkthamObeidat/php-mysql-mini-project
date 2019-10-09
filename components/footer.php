d85d930e8ecfe71c39417b1895e4ff569b0d1c04
Mohamad Aktham Obeidat <mohamadaktham7@gmail.com>
2019-10-09 23:07:32 +0300

    Login Form Created

--- components/footer.php
+++ components/footer.php
@@ -1,1 +1,4 @@
-<?php
+<footer class="section">
+    <div class="center grey-text"> Copyright 2020 PHP mini Project</div>
+</footer>
+</body>
--- home.php
+++
@@ -1,7 +0,0 @@
-<?php
-?>
-<!DOCTYPE html>
-<html lang="en">
-<?php include('components/header.php') ?>
-</body>
-</html>
