8736aed4d548ffb31f3ced3288a8f7dd49f0541d
Mohamad Aktham Obeidat <mohamadaktham7@gmail.com>
2019-10-09 21:06:29 +0300

    Create Components Directory

---
+++ components/header.php
@@ -0,0 +1,18 @@
+<head>
+    <title>PHP MINI PROJECT</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <style>
+
+    </style>
+</head>
+<body>
+    <nav>
+        <div class="container">
+            <a href="#" class="brand-logo"></a>
+            <ul class="right hide-on-small-and-down">
+                <li><a href="#" ></li>
+                <li><a href="#" ></li>
+                <li><a href="#" ></li>
+            </ul>
+        </div>
+    </nav>
